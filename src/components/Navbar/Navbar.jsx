1f617cf6d8f3b752d2da290f66587b21075011c2
jerNastri <gerardo.nastri@iismargheritahackbaronissi.edu.it>
2022-11-16 11:25:14 +0100

    first

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import {IoIosArrowDown} from 'react-icons/io'
+import {IoIosArrowUp} from 'react-icons/io'
+
+const Navbar = () => {
+
+  const [showDropdown, setShowDropdown] = useState(false)
+
+  return (
+    <div className='navbar'>
+      <div className="navbar__wrapper">
+        <h1>KOPPEE</h1>
+        <div className="navbar__links">
+          <a href="/" className='currentPage'>Home</a>
+          <a href="/about">About</a>
+          <a href="/service">Service</a>
+          <div className="navbar__dropdown" onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
+            <span>Pages <IoIosArrowDown /></span>
+            <div className={showDropdown ? "navbar__dropdown-list show"  : "navbar__dropdown-list"}>
+              <a href="/">Reservation</a>
+              <a href="/">Testimonial</a>
+            </div>
+          </div>
+          <a href="/menu">Menu</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
